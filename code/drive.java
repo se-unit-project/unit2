2cd59fbfd765c4c97d9ab7832667cddff73b09cf
hari <harikn77@gmail.com>
2022-03-20 19:06:38 +0530

    Task 2&7

--- code/drive.java
+++ code/drive.java
@@ -1,6 +1,9 @@
 import model.ControlDesk;
+import util.Setup;
 import viewcontrol.ControlDeskView;
+import viewcontrol.NotifyUser;
 
+import java.io.*;
 /**
  * drive serves as the startup file for the application.
  * 
@@ -8,14 +11,26 @@ import viewcontrol.ControlDeskView;
  */
 public class drive {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Setup setup = new Setup();
+		NotifyUser notify = new NotifyUser();
 
-		final int NUM_LANES = 3;
-		final int MAX_PATRONS_PER_ALLEY = 10;
+		setup.readConfig();
 
-		// Create a control desk and run the GUI
-		ControlDesk controlDesk = new ControlDesk(NUM_LANES);
-		ControlDeskView cdv = new ControlDeskView( controlDesk, MAX_PATRONS_PER_ALLEY);
-		controlDesk.addObserver(cdv);
+		int numLanes = setup.getNumLanes();;
+		int maxPatronsPerParty = setup.getMaxPatronsPerParty();
+
+		if(maxPatronsPerParty > 6){
+			notify.tooManyUsers();
+			System.exit(0);
+		}
+		else {
+			// Create a control desk and run the GUI
+			ControlDesk controlDesk = new ControlDesk(numLanes);
+			ControlDeskView cdv = new ControlDeskView( controlDesk, maxPatronsPerParty);
+			controlDesk.addObserver(cdv);
+		}
 	}
 }
+
+
